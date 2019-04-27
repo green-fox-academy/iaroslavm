a526ba367e731b4ce0426456cd3cb8bbb9054e63
Iaroslav Miller <iaroslav.miller@gmail.com>
2019-04-27 23:11:48 +0200

    fix(CAB): add CAB, still not ideal because the division into figures is in reverse order so 1234 and 00001234 will give 4 cows

--- week-04/day-4/src/CAB.java
+++ week-04/day-4/src/CAB.java
@@ -1,4 +1,70 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class CAB {
+    public static void main(String[] args) {
+        int guessNum = (int) (1000 + Math.random() * (9000));
+        System.out.println(guessNum);
+
+        play(guessNum);
+    }
+
+    public static void play(int guessNum) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please provide guess of 4 digit number:");
+        int number = scanner.nextInt();
+        List<Integer> guessList = new ArrayList<>();
+        List<Integer> numList = new ArrayList<>();
+
+        int guessNumCopy = guessNum;
+        int NumCopy = number;
+
+        int index = 0;
+        do {
+            guessList.add(guessNum % 10);
+            numList.add(number % 10);
+            guessNum /= 10;
+            number /= 10;
+            index++;
+        } while (number != 0);
+
+        int cowCount = 0;
+        int bullCount = 0;
+
+        //Count cows
+        index = 0;
+        int iter = 0;
+        int iterations = numList.size();
+        while (iter < iterations) {
+            if (guessList.get(index).equals(numList.get(index))) {
+                cowCount++;
+                guessList.remove(index);
+                numList.remove(index);
+            } else {
+                index++;
+            }
+            iter++;
+        }
+
+        iter = 0;
+        iterations = numList.size();
+        int indexGuess = 0;
+        while (iter < iterations){
+            for (int i = 0; i < guessList.size(); i++){
+                if (guessList.get(indexGuess).equals(numList.get(i))){
+                    bullCount++;
+                    guessList.remove(indexGuess);
+                    numList.remove(i);
+                    indexGuess--;
+                    break;
+                }
+            }
+            indexGuess++;
+            iter++;
+        }
+
+        System.out.println("Given secret number " + guessNumCopy + " and a guess of " + NumCopy
+        + " there are " + cowCount + " cows and " + bullCount + " bulls.");
+    }
 }
