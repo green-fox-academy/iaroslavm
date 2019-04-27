1b3b5124942d063b4903c7bd10cfe978558f2944
Iaroslav Miller <iaroslav.miller@gmail.com>
2019-04-27 23:30:42 +0200

    feat(CabTests): add test for CCAB that errors when longer number producer result as if the guess is correct

--- week-04/day-4/tests/AnimalTest.java
+++ week-04/day-4/tests/AnimalTest.java
@@ -1,10 +1,36 @@
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class AnimalTest {
     Animal myAnimal;
 
     @Test
     public void test1(){
+        myAnimal = new Animal(100,100);
+        for(int i = 0; i < 50; i++){
+            myAnimal.eat();
+            myAnimal.drink();
+        }
+        assertEquals(50, myAnimal.getHunger());
+    }
 
+    @Test
+    public void test2(){
+        myAnimal = new Animal(100,100);
+        for(int i = 0; i < 50; i++){
+            myAnimal.eat();
+            myAnimal.drink();
+        }
+        assertEquals(50, myAnimal.getThirst());
+    }
+
+    @Test
+    public void test3(){
+        myAnimal = new Animal(100,100);
+        for(int i = 0; i < 50; i++){
+            myAnimal.play();
+        }
+        assertEquals(150, myAnimal.getHunger());
     }
 }
--- week-04/day-4/tests/CabTest.java
+++ week-04/day-4/tests/CabTest.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 public class CabTest {
+  CAB cab;
+  int number;
+
+  @Test
+  public void checkCAB() {
+    cab = new CAB();
+    assertEquals(8,cab.play(2222,2222));
+  }
+
+  @Test
+  public void checkCABlong() {
+    cab = new CAB();
+    assertNotEquals(8,cab.play(2222,1112222));
+  }
 }
