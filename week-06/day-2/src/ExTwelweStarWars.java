68c1463260dc3fbedfe1953ac2a435aa38bd6ebb
Iaroslav Miller <iaroslav.miller@gmail.com>
2019-05-14 18:32:02 +0200

    feat(ex 12): add ex 12, not fully completed yet

--- week-06/day-2/src/ExTwelweStarWars.java
+++ week-06/day-2/src/ExTwelweStarWars.java
@@ -1,26 +1,81 @@
 import java.io.IOException;
+import java.lang.invoke.SwitchPoint;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toMap;
 
 public class ExTwelweStarWars {
 
-  public static void getResult(){
+  public static void getResult() {
+
+    System.out.println("Max weight belongs to "+  checkWeight(readFile()));
+    System.out.println("Average male height is  "+  checkMaleHeight(readFile()));
 
   }
-  public static void readFile(){
+
+  public static List<SWCharacter> readFile() {
     Path filePath = Paths.get("swcharacters.csv");
+    List<SWCharacter> swPeople = new ArrayList<>();
     try {
       List<String> logFile = Files.readAllLines(filePath);
-      List<String> newList = new ArrayList<>();
 
-      return newList;
+      String[] features;
+      for (int i = 1; i < logFile.size(); i++) {
+        features = logFile.get(i).split(";");
+        swPeople.add(new SWCharacter(features[0],
+            features[1],
+            features[2].replaceAll(",",""),
+            features[3],
+            features[4],
+            features[5],
+            features[6],
+            features[7]
+        ));
+      }
+      return swPeople;
     } catch (IOException e) {
       e.printStackTrace();
-      return null;
+      return swPeople;
     }
+  }
+
+  public static String checkWeight(List<SWCharacter> heroes) {
+    System.out.println(heroes.get(0).getMass());
+
+    List<Map.Entry<String, Double>> sortedWeigthMap =
+        heroes
+            .stream()
+            .filter(e -> !e.getMass().equals("n/a"))
+            .filter(e -> !e.getMass().equals("none"))
+            .filter(e -> !e.getMass().equals("unknown"))
+            .collect(
+                toMap(e -> e.getName(), e -> Double.parseDouble(e.getMass()), (e1, e2) -> e2,
+                    LinkedHashMap::new))
+            .entrySet()
+            .stream()
+            .sorted(Collections.reverseOrder(Map.Entry.comparingByValue()))
+            .collect(Collectors.toList());
+
+    return sortedWeigthMap.get(0).getKey();
+  }
+
+  public static double checkMaleHeight(List<SWCharacter> heroes){
+    double meanHeight =
+        heroes
+            .stream()
+            .filter(e -> !e.getMass().equals("n/a"))
+            .filter(e -> !e.getMass().equals("none"))
+            .filter(e -> !e.getMass().equals("unknown"))
+            .filter(e -> e.getGender().toLowerCase().equals("male"))
+            .map(h -> h.getHeight())
+            .map(h -> Integer.parseInt(h))
+            .mapToInt(h -> h)
+            .average()
+            .orElse(Double.NaN);
+    return meanHeight;
   }
 }
--- week-06/day-2/src/SWCharacter.java
+++ week-06/day-2/src/SWCharacter.java
@@ -1,7 +1,7 @@
-public class SWCharacter {
+public class SWCharacter{
   private String name;
-  private int height;
-  private int mass;
+  private String height;
+  private String mass;
   private String hair_color;
   private String skin_color;
   private String eye_color;
@@ -10,21 +10,86 @@ public class SWCharacter {
 
   public SWCharacter(
       String name,
-      int height,
-      int mass,
+      String height,
+      String mass,
       String hair_color,
       String skin_color,
       String eye_color,
       String birth_year,
       String gender
   ) {
-    this.name,
-        this.height,
-        this.mass,
-        this.hair_color,
-        this.skin_color,
-        this.eye_color,
-        this.birth_year,
-        this.gender
+    this.name = name;
+        this.height = height;
+        this.mass = mass;
+        this.hair_color = hair_color;
+        this.skin_color = skin_color;
+        this.eye_color = eye_color;
+        this.birth_year = birth_year;
+        this.gender = gender;
   }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getHeight() {
+    return height;
+  }
+
+  public void setHeight(String height) {
+    this.height = height;
+  }
+
+  public String getMass() {
+    return mass;
+  }
+
+  public void setMass(String mass) {
+    this.mass = mass;
+  }
+
+  public String getHair_color() {
+    return hair_color;
+  }
+
+  public void setHair_color(String hair_color) {
+    this.hair_color = hair_color;
+  }
+
+  public String getSkin_color() {
+    return skin_color;
+  }
+
+  public void setSkin_color(String skin_color) {
+    this.skin_color = skin_color;
+  }
+
+  public String getEye_color() {
+    return eye_color;
+  }
+
+  public void setEye_color(String eye_color) {
+    this.eye_color = eye_color;
+  }
+
+  public String getBirth_year() {
+    return birth_year;
+  }
+
+  public void setBirth_year(String birth_year) {
+    this.birth_year = birth_year;
+  }
+
+  public String getGender() {
+    return gender;
+  }
+
+  public void setGender(String gender) {
+    this.gender = gender;
+  }
+
 }
