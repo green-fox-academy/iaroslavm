049b9c2d6053c305abe5f6a6a105246154446744
Iaroslav Miller <iaroslav.miller@gmail.com>
2019-05-23 21:28:30 +0200

    feat(GreenFox Refactoring): add application.java for Fox Club

---
+++ week-077/fox-toy-project/src/main/java/com/foxtoy/foxtoydemo/FoxtoydemoApplication.java
@@ -0,0 +1,13 @@
+package com.foxtoy.foxtoydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoxtoydemoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FoxtoydemoApplication.class, args);
+  }
+
+}
