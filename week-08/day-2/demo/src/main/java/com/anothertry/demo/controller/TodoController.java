64950d299c39fe8ac8a405687574636c5d9dd2a0
Iaroslav Miller <iaroslav.miller@gmail.com>
2019-05-28 20:09:55 +0200

    feat(toto): add adding section

--- week-08/day-2/demo/src/main/java/com/anothertry/demo/controller/TodoController.java
+++ week-08/day-2/demo/src/main/java/com/anothertry/demo/controller/TodoController.java
@@ -5,29 +5,27 @@ import com.anothertry.demo.repository.ITodoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Controller
-@RequestMapping("todo")
+@RequestMapping("/todo")
+
 public class TodoController {
 
   @Autowired
   ITodoRepository repository;
 
   @RequestMapping(value = {"/list", "/"}, method = RequestMethod.GET)
-  public String list(Model model, @RequestParam(name="isActive", required = false) String isActive){
+  public String list(Model model, @RequestParam(name = "isActive", required = false) String isActive) {
     List<Todo> todos = new ArrayList<>();
     repository.findAll().forEach(todos::add);
 
-    if(isActive != null) {
-      if(isActive.equals("true")){
+    if (isActive != null) {
+      if (isActive.equals("true")) {
         model.addAttribute("todos", todos.stream().filter(todo -> !todo.isDone()).collect(Collectors.toList()));
       } else {
         model.addAttribute("todos", todos.stream().filter(Todo::isDone).collect(Collectors.toList()));
@@ -39,5 +37,16 @@ public class TodoController {
     return "listtodo";
   }
 
+  @GetMapping("/add")
+  public String addGet(Model model) {
+    model.addAttribute("newTodo", new Todo());
+    return "add";
+  }
+
+  @PostMapping("/add")
+  public String addPost(@ModelAttribute Todo newTodo) {
+    repository.save(newTodo);
+    return "redirect:/todo/";
+  }
 
 }
