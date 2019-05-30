f5d27974e50be11c6197d0c01a36d97f283d89b5
Iaroslav Miller <iaroslav.miller@gmail.com>
2019-05-30 14:29:33 +0200

    fix(ToyFox): check commit

--- week-077/fox-toy-project/src/main/java/com/foxtoy/foxtoydemo/controllers/MainController.java
+++ week-077/fox-toy-project/src/main/java/com/foxtoy/foxtoydemo/controllers/MainController.java
@@ -8,6 +8,7 @@ import com.foxtoy.foxtoydemo.services.FoxServices;
 import com.foxtoy.foxtoydemo.services.FoxServicesImp;
 //import com.foxtoy.foxtoydemo.services.SaveService;
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
