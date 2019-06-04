22ac7e222568f2fccb31b061738ffe11a42768c7
Iaroslav Miller <iaroslav.miller@gmail.com>
2019-06-04 17:25:32 +0200

    feat(trialExam): commit save-link with 3 scenarios

---
+++ week-09/trialexam/src/main/java/com/trialexam/trialexam/controller/HomeController.java
@@ -0,0 +1,55 @@
+package com.trialexam.trialexam.controller;
+
+import com.trialexam.trialexam.model.UrlClass;
+import com.trialexam.trialexam.service.IUrlService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Random;
+
+@Controller
+public class HomeController {
+
+  private IUrlService urlService;
+
+  public HomeController(IUrlService urlService) {
+    this.urlService = urlService;
+  }
+
+  @GetMapping("/")
+  public String getIndex(Model model, String urlName, String aliasName, String newUrlID){
+    if(urlName == null & newUrlID == null){
+      return "index";
+    } else if (newUrlID != null){
+      model.addAttribute("aliasName", urlService.findAllById(Long.parseLong(newUrlID)).getAlias());
+      model.addAttribute("codeName", urlService.findAllById(Long.parseLong(newUrlID)).getSecretCode());
+      return "index";
+    } else {
+      model.addAttribute("existingUrlName",urlName);
+      model.addAttribute("existingAliasName",aliasName);
+      return "index";
+    }
+  }
+
+  @PostMapping("/save-link")
+  public String saveLink(String urlName, String aliasName){
+    if(urlService.checkIfAliasNameExists(aliasName)){
+      return "redirect:/?urlName="
+          + urlName
+          + "&aliasName="
+          + aliasName;
+    } else {
+      UrlClass newUrlObject = new UrlClass();
+      newUrlObject.setSecretCode(
+          String.format("%04d", new Random().nextInt(10000)));
+      newUrlObject.setUrl(urlName);
+      newUrlObject.setAlias(aliasName);
+      urlService.save(newUrlObject);
+      return "redirect:/?newUrlID=" + newUrlObject.getId();
+    }
+
+  }
+}
